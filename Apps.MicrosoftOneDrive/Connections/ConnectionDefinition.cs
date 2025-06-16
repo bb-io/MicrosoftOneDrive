e7811f395d30900201118fd8b9967fb8e19be1c6
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-06-16 11:44:19 +0300

    chore(version): 🔧 update version to 1.2.5; refactor `ConnectionPropertyGroups` for improved readability; streamline `CreateAuthorizationCredentialsProviders` method

--- Apps.MicrosoftOneDrive/Connections/ConnectionDefinition.cs
+++ Apps.MicrosoftOneDrive/Connections/ConnectionDefinition.cs
@@ -5,22 +5,19 @@ namespace Apps.MicrosoftOneDrive.Connections;
 
 public class ConnectionDefinition : IConnectionDefinition
 {
-    public IEnumerable<ConnectionPropertyGroup> ConnectionPropertyGroups => new List<ConnectionPropertyGroup>
-    {
-        new ConnectionPropertyGroup
+    public IEnumerable<ConnectionPropertyGroup> ConnectionPropertyGroups =>
+    [
+        new()
         {
             Name = "OAuth",
             AuthenticationType = ConnectionAuthenticationType.OAuth2,
             ConnectionProperties = new List<ConnectionProperty> { }
         },
-    };
+    ];
 
     public IEnumerable<AuthenticationCredentialsProvider> CreateAuthorizationCredentialsProviders(Dictionary<string, string> values)
     {
         var token = values.First(v => v.Key == "access_token");
-        yield return new AuthenticationCredentialsProvider(
-            "Authorization",
-            $"Bearer {token.Value}"
-        );
+        yield return new AuthenticationCredentialsProvider("Authorization", $"Bearer {token.Value}");
     }
 }
